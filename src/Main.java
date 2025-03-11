6d20802e8368c6cda2d7293af0312c7b161e238b
jieun615 <je060105@gmail.com>
2025-03-11 16:31:50 +0900

    키오스크 과제 Lv3 완료

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
+import kioskpackage.Kiosk;
 import kioskpackage.MenuItem;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,38 +13,7 @@ public class Main {
         menuItems.add(new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
         menuItems.add(new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
 
-        Scanner scanner = new Scanner(System.in);
-
-        while (true) {
-            System.out.println("\n[ SHAKESHACK MENU ]");
-            for(int i = 0; i < menuItems.size(); i++) {
-                System.out.println((i + 1) + ". " + menuItems.get(i));
-            }
-            System.out.println("0. 종료      | 종료");
-            System.out.print("메뉴 번호를 입력하세요: ");
-            int num = scanner.nextInt();
-
-            switch (num) {
-                case 1:
-                    System.out.println("1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거");
-                    break;
-                case 2:
-                    System.out.println("2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
-                    break;
-                case 3:
-                    System.out.println("3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
-                    break;
-                case 4:
-                    System.out.println("4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
-                    break;
-                case 0:
-                    System.out.println("프로그램을 종료합니다.");
-                    scanner.close();
-                    return;
-                default:
-                    System.out.println("없는 메뉴입니다.");
-                    break;
-            }
-        }
+        Kiosk kiosk = new Kiosk(menuItems);
+        kiosk.start();
     }
 }
